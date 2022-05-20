124f5c2ba07c20248c93dde9a61ced676a5fa85c
Chriss0510 <105904401+Chriss0510@users.noreply.github.com>
2022-05-20 12:30:52 +0700

    first upload

---
+++ java.js
@@ -0,0 +1,179 @@
+let Gadget = [
+
+{
+    kode: "BRG001",
+    nama: "Iphone 13 Pro Max",
+    stock: "10",
+    rating: "5.0",
+    foto: "https://imgsrv2.voi.id/8owd_35AglozUf0_CmJbTd3h4HX4Q1gzfe0Z8H3-_0s/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xMzc0MzkvMjAyMjAyMjExNjQ2LW1haW4uY3JvcHBlZF8xNjQ1NDM2Nzg5LkpQRw.jpg",
+    harga: "22000000",
+},
+{
+    kode: "BRG002",
+    nama: "Samsung A7 Lite",
+    stock: "20",
+    rating: "5.0",
+    foto: "https://img.tek.id/img/content/2021/03/29/39007/samsung-akan-luncurkan-galaxy-tab-a7-lite-dan-s7-lite-aq0ux1T5XV.jpg",
+    harga: "50000000",
+},
+{
+    kode: "BRG003",
+    nama: "Oppo Reno 9",
+    stock: "100",
+    rating: "5.0",
+    foto: "https://jasystore.com/wp-content/uploads/2021/12/oppo-reno8-pro-5g-1-500x500.jpg",
+    harga: "70000000",
+},
+{
+    kode: "BRG004",
+    nama: "Xiaomi Redmi Note 11",
+    stock: "1000",
+    rating: "5.0",
+    foto: "https://i01.appmifile.com/webfile/globalimg/products/pc/redmi-note-11/ipgone003.png",
+    harga: "50000000",
+},
+{
+    kode: "BRG005",
+    nama: "Vivo X70 Pro",
+    stock: "50",
+    rating: "5.0",
+    foto: "https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/product/1638158550713/zip/img/pc_scale_phone.png",
+    harga: "60000000",
+},
+{
+    kode: "BRG006",
+    nama: "LG",
+    stock: "10",
+    rating: "7.0",
+    foto: "https://www.lg.com/id/images/tv/md07523816/gallery/D_01_v1.jpg",
+    harga: "2000000",
+},
+{
+    kode: "BRG007",
+    nama: "Hp",
+    stock: "20",
+    rating: "7.0",
+    foto: "https://i5.walmartimages.com/asr/8dced38f-bacb-4ce1-ab7a-57f10921e07a_1.2f94685b5c9826a7affc4f55807bd6f4.jpeg",
+    harga: "5000000",
+},
+{
+    kode: "BRG008",
+    nama: "Samsung",
+    stock: "100",
+    rating: "7.0",
+    foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmy-best.id%2F138730&psig=AOvVaw3ZNnHXxTSNNbWeFx7hDBle&ust=1650343007750000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCNiKnIrlnPcCFQAAAAAdAAAAABAD",
+    harga: "7000000",
+},
+{
+    kode: "BRG009",
+    nama: "Asus",
+    stock: "1000",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.suara.com%2Fpictures%2F970x544%2F2019%2F06%2F17%2F26291-pc-asus-pro-d340mc.jpg&imgrefurl=https%3A%2F%2Fwww.suara.com%2Ftekno%2F2019%2F06%2F17%2F212251%2Fpc-asus-pro-d340mc-diluncurkan-diklaim-cocok-untuk-umkm&tbnid=wSs058iKGcAlhM&vet=12ahUKEwiow-KT5Zz3AhXTxKACHRtWBNIQMygHegUIARDmAQ..i&docid=usOF3TQ1_mP0SM&w=970&h=544&q=komputer%20asus&ved=2ahUKEwiow-KT5Zz3AhXTxKACHRtWBNIQMygHegUIARDmAQ",
+    harga: "5000000",
+},
+{
+    kode: "BRG0010",
+    nama: "ROG",
+    stock: "50",
+    rating: "7.0",
+    foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fkopitekno.com%2Fasus-rog-desktop-terbaru-strix-ga35-ga15%2F&psig=AOvVaw0XZR086nYCFurI39Q8LhIO&ust=1650343092804000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCLj4n6PlnPcCFQAAAAAdAAAAABAU",
+    harga: "6000000",
+},
+{
+    kode: "BRG006",
+    nama: "Baju Bola",
+    stock: "10",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcf.shopee.co.id%2Ffile%2Fcf8654e9ad438f902a815c4486933dca&imgrefurl=https%3A%2F%2Fshopee.co.id%2FKaos-Jersey-Futsal-Baju-Bola-Custom-Nama-dan-Nomor-Full-Print-08-i.34307332.2102952178&tbnid=esJUTAK4bEFBdM&vet=12ahUKEwj59Iy16Jz3AhUii9gFHSPFAQUQMygAegUIARDXAQ..i&docid=Aku3WCnB9otrWM&w=600&h=600&q=baju%20bola&ved=2ahUKEwj59Iy16Jz3AhUii9gFHSPFAQUQMygAegUIARDXAQ",
+    harga: "2000000",
+},
+{
+    kode: "BRG007",
+    nama: "Baju Basket",
+    stock: "20",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi0.wp.com%2Fmaestrojersey.com%2Fwp-content%2Fuploads%2F2021%2F03%2Fjersey-basket-printing.jpg%3Ffit%3D1670%252C1670%26ssl%3D1&imgrefurl=https%3A%2F%2Fmaestrojersey.com%2Fpembuatan-jersey-basket%2F&tbnid=pnwB2akClk6Z6M&vet=12ahUKEwizyZvD6Jz3AhW20nMBHRInA9MQMygEegUIARDnAQ..i&docid=Pi8GCH-RizvNjM&w=1670&h=1670&q=baju%20basket&ved=2ahUKEwizyZvD6Jz3AhW20nMBHRInA9MQMygEegUIARDnAQ",
+    harga: "5000000",
+},
+{
+    kode: "BRG008",
+    nama: "Baju lari",
+    stock: "100",
+    rating: "7.0",
+    foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tokopedia.com%2Fsportwearr%2Funique-kaos-lari-pria-kaos-olahraga-nb-kaos-bola-futsal-t-shirt-nb-abu-muda-xl&psig=AOvVaw1zDzzBFt6TVx37Wf-jcm2j&ust=1650344009118000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCOC2vNfonPcCFQAAAAAdAAAAABAD",
+    harga: "7000000",
+},
+{
+    kode: "BRG009",
+    nama: "Baju Renang",
+    stock: "1000",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcf.shopee.co.id%2Ffile%2F0bc8cd37e88faf0baf910ec4c1e2963b&imgrefurl=https%3A%2F%2Fshopee.co.id%2FBAJU-RENANG-ANAK-LAKI-USIA-4-8-TAHUN-BAJU-DIVING-BAJU-RENANG-ANAK-SD-LAKI-BAJU-RENANG-ANAK-i.15348023.600955437&tbnid=syumwUToTv8e_M&vet=12ahUKEwj61KLg6Jz3AhWYktgFHWIdCT4QMygAegUIARDaAQ..i&docid=RHtqcT_TTINcWM&w=875&h=875&q=baju%20renang&ved=2ahUKEwj61KLg6Jz3AhWYktgFHWIdCT4QMygAegUIARDaAQ",
+    harga: "5000000",
+},
+{
+    kode: "BRG0010",
+    nama: "Baju Badminton",
+    stock: "50",
+    rating: "7.0",
+    foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.priceza.co.id%2Fs%2Fharga%2Fkaos-badminton-pink&psig=AOvVaw2dDlJX20FOpv3jJOpMDXNn&ust=1650344054589000&source=images&cd=vfe&ved=0CAkQjRxqFwoTCMDegPHonPcCFQAAAAAdAAAAABAD",
+    harga: "10000000",
+},
+{
+    kode: "BRG006",
+    nama: "Ps1",
+    stock: "10",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn1-production-images-kly.akamaized.net%2Fc8TqBjT3BRmt0JSwhxV8v2452sI%3D%2F1200x675%2Fsmart%2Ffilters%3Aquality(75)%3Astrip_icc()%3Aformat(jpeg)%2Fkly-media-production%2Fmedias%2F1600117%2Foriginal%2F077059000_1495266344-01-playstation.jpg&imgrefurl=https%3A%2F%2Fwww.liputan6.com%2Ftekno%2Fread%2F3283410%2F10-gim-ps1-terlaris-mana-yang-pernah-kamu-mainkan&tbnid=LpWhl_KtkinuhM&vet=12ahUKEwi5xrub5pz3AhX6KbcAHftVDhUQMygBegUIARDkAQ..i&docid=Ph-4JopMLAkhfM&w=1200&h=675&q=PS1&ved=2ahUKEwi5xrub5pz3AhX6KbcAHftVDhUQMygBegUIARDkAQ",
+    harga: "2000000",
+},
+{
+    kode: "BRG007",
+    nama: "Ps2",
+    stock: "20",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn0-production-images-kly.akamaized.net%2Fp93B9lRWuGlPF87X-2qgk_fHP7A%3D%2F1200x675%2Fsmart%2Ffilters%3Aquality(75)%3Astrip_icc()%3Aformat(jpeg)%2Fkly-media-production%2Fmedias%2F1768438%2Foriginal%2F088569200_1510560514-ps2.jpg&imgrefurl=https%3A%2F%2Fwww.liputan6.com%2Ftekno%2Fread%2F4194788%2Fulang-tahun-ke-20-ps2-masih-mantap-jadi-konsol-terlaris-hingga-kini&tbnid=xmAhVyehQuaDcM&vet=12ahUKEwiywdak5pz3AhWYk9gFHbyVBYkQMygBegUIARDeAQ..i&docid=cHdXevKCO6HvLM&w=1200&h=675&q=PS2&ved=2ahUKEwiywdak5pz3AhWYk9gFHbyVBYkQMygBegUIARDeAQ",
+    harga: "5000000",
+},
+{
+    kode: "BRG008",
+    nama: "Ps3",
+    stock: "100",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi5.walmartimages.com%2Fasr%2Fa6f484c4-2f49-45e0-a7d6-6e47630214ba_1.b995164ff84ad1991f947204a82c2bea.jpeg%3FodnHeight%3D612%26odnWidth%3D612%26odnBg%3DFFFFFF&imgrefurl=https%3A%2F%2Fwww.walmart.com%2Fip%2FRefurbished-Sony-PlayStation-3-PS3-500GB-Console-Red%2F891539653&tbnid=T1Pi_2g9Hd3ZTM&vet=12ahUKEwj0qO2s5pz3AhXGmtgFHROyCcYQMygSegUIARD9AQ..i&docid=e4d1bXR0wnO57M&w=500&h=500&q=PS3&ved=2ahUKEwj0qO2s5pz3AhXGmtgFHROyCcYQMygSegUIARD9AQ",
+    harga: "7000000",
+},
+{
+    kode: "BRG009",
+    nama: "Ps4",
+    stock: "1000",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F8%2F8c%2FPS4-Console-wDS4.png%2F1200px-PS4-Console-wDS4.png&imgrefurl=https%3A%2F%2Fwww.pegasusaerogroup.com%2Fcategory%2Fnoticias%2Fpage%2F2%2F%3Fss%3D5_4_1_17_41%26pp%3Dps4%26ii%3D1612398&tbnid=pcvFb_mPf-TnzM&vet=12ahUKEwiXqKvD5pz3AhVzjtgFHVDHB2cQMygEegUIARDkAQ..i&docid=vI1qQDsIbCqA7M&w=1200&h=600&q=PS4&ved=2ahUKEwiXqKvD5pz3AhVzjtgFHVDHB2cQMygEegUIARDkAQ",
+    harga: "5000000",
+},
+{
+    kode: "BRG0010",
+    nama: "Ps5",
+    stock: "50",
+    rating: "7.0",
+    foto: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fasset.kompas.com%2Fcrops%2FOShkHBI40cCFj6mMFFcYmhbhBaw%3D%2F187x12%3A1126x638%2F750x500%2Fdata%2Fphoto%2F2020%2F06%2F12%2F5ee2bae6901d6.jpg&imgrefurl=https%3A%2F%2Ftekno.kompas.com%2Fread%2F2020%2F06%2F13%2F17020007%2Fini-spesifikasi-hardware-playstation-5&tbnid=9afEAn-xhejK_M&vet=12ahUKEwiqudjO5pz3AhWEjtgFHZ74D4EQMygBegUIARDZAQ..i&docid=t3y0B6QLCpms6M&w=750&h=500&q=PS5&ved=2ahUKEwiqudjO5pz3AhWEjtgFHZ74D4EQMygBegUIARDZAQ",
+    harga: "10000000",
+}
+
+];
+console.log(Gadget);
+
+function carigadget(nama) {
+    for (index in Gadget) {
+        if(Gadget[index].nama==nama){
+
+        }
+    }
+}
+
+if(carigadget(prompt("Masukan nama barang"))){
+    alert("ketemu");
+} else {
+    alert("Gak ketemu");
+}
